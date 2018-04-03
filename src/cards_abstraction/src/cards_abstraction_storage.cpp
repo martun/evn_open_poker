2c5bfe8635577dba3d7eccca9d3fdc6929f99244
Levon <levon_petrosyan@edu.aua.am>
2018-04-03 16:23:41 +0400

    Saving and loading strings into and from  bin. file functions implementation

--- src/cards_abstraction/src/cards_abstraction_storage.cpp
+++ src/cards_abstraction/src/cards_abstraction_storage.cpp
@@ -26,52 +26,202 @@ void CardsAbstractionStorage::save(const std::string& file_path) const {
   save_indexes_to_string(flop_index_table, flopIndexTable);
   save_indexes_to_string(turn_index_table, turnIndexTable);
   save_indexes_to_string(river_index_table, riverIndexTable);
-  
-  //    std::ofstream out("output.txt");
-  //    out << preflopCardTable<<"\n";
-  //    out << flopCardTable<<"\n";
-  //    out<< turnCardTable<<"\n";
-  //    out<< riverCardTable<<"\n";
-  //    out<<preflopIndexTable<<"\n";
-  //    out<<flopIndexTable<<"\n";
-  //    out<< turnIndexTable<<"\n";
-  //    out<<riverIndexTable<<"\n";
-  //    out.close();
+
+      FILE*  out=fopen((const char*)file_path,"w");
+    // writing the sizes into file
+    uint32_t preflopCTableSize = preflopCardTable.size();
+    fwrite (&preflopCTableSize
+              , sizeof(uint32_t)
+              , 1, out );
+    uint32_t flopCTableSize = flopCardTable.size();
+    fwrite (&flopCTableSize
+            , sizeof(uint32_t)
+            , 1, out );
+    uint32_t turnCTableSize = turnCardTable.size();
+    fwrite (&turnCTableSize
+            , sizeof(uint32_t)
+            , 1, out );
+    uint32_t riverCTableSize = riverCardTable.size();
+    fwrite (&riverCTableSize
+            , sizeof(uint32_t)
+            , 1, out );
+    uint32_t preflopITableSize = preflopCardTable.size();
+    fwrite (&preflopITableSize
+            , sizeof(uint32_t)
+            , 1, out );
+    uint32_t flopITableSize = flopCardTable.size();
+    fwrite (&flopITableSize
+            , sizeof(uint32_t)
+            , 1, out );
+    uint32_t turnITableSize = turnCardTable.size();
+    fwrite (&turnITableSize
+            , sizeof(uint32_t)
+            , 1, out );
+    uint32_t riverITableSize = riverCardTable.size();
+    fwrite (&riverITableSize
+            , sizeof(uint32_t)
+            , 1, out );
+    //writing strings into file
+    if(fwrite (preflopCardTable.c_str()
+            , sizeof(char)
+            , preflopCTableSize
+            , out )
+       !=preflopCTableSize)
+        throw "Error while writing to file";
+    if(fwrite (flopCardTable.c_str()
+            , sizeof(char)
+            , flopCTableSize
+            , out )
+        !=flopCTableSize)
+        throw "Error while writing to file";
+    if(fwrite (turnCardTable.c_str()
+            , sizeof(char)
+            , turnCTableSize
+            , out )
+        !=turnCTableSize)
+        throw "Error while writing to file";
+    if(fwrite (riverCardTable.c_str()
+            , sizeof(char)
+            , riverCTableSize
+            , out )
+        !=riverCTableSize)
+        throw "Error while writing to file";
+    if(fwrite (preflopIndexTable.c_str()
+            , sizeof(char)
+            , preflopITableSize
+            , out )
+        !=preflopITableSize)
+        throw "Error while writing to file";
+    if(fwrite (flopIndexTable.c_str()
+            , sizeof(char)
+            , flopITableSize
+            , out )
+        !=flopITableSize)
+        throw "Error while writing to file";
+    if(fwrite (turnIndexTable.c_str()
+            , sizeof(char)
+            , turnITableSize
+            , out )
+        !=turnITableSize)
+        throw "Error while writing to file";
+    if(fwrite (riverIndexTable.c_str()
+            , sizeof(char)
+            , riverITableSize
+            , out )
+        !=riverITableSize)
+        throw "Error while writing to file";
+      fclose(out);
 }
 
 void CardsAbstractionStorage::load(const std::string& file_path) {
-  //    std::ifstream ifile("output.txt");  //if file existes
-  //    if(!ifile){
-  (new CardsAbstractionBuilder(*this))->build_all();
-  //    }
-  //    else{
-  //        std::string preflopCardTable; //get stringes from somewhere
-  //        std::string flopCardTable;
-  //        std::string turnCardTable;
-  //        std::string riverCardTable;
-  //        std::ifstream infile;
-  //        infile.open ("output.txt");
-  //        getline(infile,preflopCardTable);
-  //        getline(infile,flopCardTable);
-  //        getline(infile,turnCardTable);
-  //        getline(infile,riverCardTable);
-  //
-  //        load_cards_from_string(preflopCardTable,PREFLOP,preflop_card_table);
-  //        load_cards_from_string(flopCardTable,FLOP,flop_card_table);
-  //        load_cards_from_string(turnCardTable,TURN,turn_card_table);
-  //        load_cards_from_string(riverCardTable,RIVER,river_card_table);
-  //        int size=flop_card_table.size();
-  //         size=turn_card_table.size();
-  //         size=river_card_table.size();
-  //        std::string preflopIndexTable;
-  //        std::string flopIndexTable;
-  //        std::string turnIndexTable;
-  //        std::string riverIndexTable;
-  //        load_indexes_from_string(preflopIndexTable,preflop_index_table);
-  //        load_indexes_from_string(flopIndexTable,flop_index_table);
-  //        load_indexes_from_string(turnIndexTable,turn_index_table);
-  //        load_indexes_from_string(riverIndexTable,river_index_table);
-  //    }
+      FILE* in=fopen((const char *) file_path, "r");  //tries to open file
+      if(in==NULL){                   //if file not exists existes
+        throw "File is not existing";
+      }
+      else{          //if file exists, get stringes from file
+          //taking sizes of strings from the file
+          uint32_t  prefCardTableSize
+                  , flopCardTableSize
+                  , turnCardTableSize
+                  , riverCardTableSize
+                  , preflopIndexTableSize
+                  , flopIndexTableSize
+                  , turnIndexTableSize
+                  , riverIndexTableSize;
+
+          std::fread(reinterpret_cast<char*>(&prefCardTableSize)
+                     , sizeof(prefCardTableSize),1,in);
+
+          std::fread(reinterpret_cast<char*>(&flopCardTableSize)
+                     , sizeof(flopCardTableSize),1,in);
+
+          std::fread(reinterpret_cast<char*>(&turnCardTableSize)
+                     , sizeof(turnCardTableSize),1,in);
+
+          std::fread(reinterpret_cast<char*>(&riverCardTableSize)
+                     , sizeof(riverCardTableSize),1,in);
+
+          std::fread(reinterpret_cast<char*>(&preflopIndexTableSize)
+                     , sizeof(preflopIndexTableSize),1,in);
+
+          std::fread(reinterpret_cast<char*>(&flopIndexTableSize)
+                     , sizeof(flopIndexTableSize),1,in);
+
+          std::fread(reinterpret_cast<char*>(&turnIndexTableSize)
+                     , sizeof(turnIndexTableSize),1,in);
+
+          std::fread(reinterpret_cast<char*>(&riverIndexTableSize)
+                     , sizeof(riverIndexTableSize),1,in);
+
+          char buffer0[prefCardTableSize]
+             , buffer1[flopCardTableSize]
+             , buffer2[turnCardTableSize]
+             , buffer3[riverCardTableSize];
+
+
+          std::fread(buffer0, sizeof(char)
+                     , prefCardTableSize,in);
+
+          std::fread(buffer1, sizeof(char)
+                     , flopCardTableSize,in);
+
+          std::fread(buffer2, sizeof(char)
+                     , turnCardTableSize,in);
+
+          std::fread(buffer3, sizeof(char)
+                     , riverCardTableSize,in);
+
+          std::string preflopCardTable(buffer0)
+                     , flopCardTable(buffer1)
+                     , turnCardTable(buffer2)
+                     , riverCardTable(buffer3);
+
+          load_cards_from_string(preflopCardTable
+                                 , PREFLOP
+                                 , preflop_card_table);
+          load_cards_from_string(flopCardTable
+                                 , FLOP
+                                 , flop_card_table);
+          load_cards_from_string(turnCardTable
+                                 , TURN
+                                 , turn_card_table);
+          load_cards_from_string(riverCardTable
+                                 , RIVER
+                                 , river_card_table);
+
+          char buffer10[preflopIndexTableSize]
+             , buffer11[flopIndexTableSize]
+             , buffer12[turnIndexTableSize]
+             , buffer13[riverIndexTableSize];
+
+
+          std::fread(buffer10, sizeof(char)
+                  ,preflopIndexTableSize,in);
+
+          std::fread(buffer11, sizeof(char)
+                  ,flopIndexTableSize,in);
+
+          std::fread(buffer12, sizeof(char)
+                  ,turnIndexTableSize,in);
+
+          std::fread(buffer13, sizeof(char)
+                  ,riverIndexTableSize,in);
+
+          std::string preflopIndexTable(buffer10);
+          std::string flopIndexTable(buffer11);
+          std::string turnIndexTable(buffer12);
+          std::string riverIndexTable(buffer13);
+
+          load_indexes_from_string(preflopIndexTable
+                                   , preflop_index_table);
+          load_indexes_from_string(flopIndexTable
+                                   , flop_index_table);
+          load_indexes_from_string(turnIndexTable
+                                   , turn_index_table);
+          load_indexes_from_string(riverIndexTable
+                                   , river_index_table);
+      }
+    fclose(in);
 }
 
 void CardsAbstractionStorage::save_indexes_to_string(
--- src/cards_abstraction/src/ehs_lut_storage.cpp
+++ src/cards_abstraction/src/ehs_lut_storage.cpp
@@ -24,31 +24,155 @@ void EhsLutStorage::save(const std::string& file_path) const {
     save_to_string(river_ehs_table,riverEhsTable);
     std::string riverEhsSquaredTable;
     save_to_string(river_ehs_squared_table,riverEhsSquaredTable);
-//    TODO put strings to file
+
+    FILE*  out=fopen((const char*)file_path,"w");
+    // writing the sizes
+    uint32_t preFlopEhsTableSize = preflopEhsTable.size();
+    fwrite (&preFlopEhsTableSize
+            , sizeof(uint32_t)
+            , 1, out );
+
+    uint32_t FlopEhsTableSize = flopEhsTable.size();
+    fwrite (&FlopEhsTableSize
+            , sizeof(uint32_t)
+            , 1, out );
+
+    uint32_t turnEhsTableSize = turnEhsTable.size();
+    fwrite (&turnEhsTableSize
+            , sizeof(uint32_t)
+            , 1, out );
+
+    uint32_t riverEhsTableSize = riverEhsTable.size();
+    fwrite (&riverEhsTableSize
+            , sizeof(uint32_t)
+            , 1, out );
+
+    //writing strings into file
+    if(fwrite (preflopEhsTable.c_str()
+            , sizeof(char)
+            , preFlopEhsTableSize
+            , out )
+        !=preFlopEhsTableSize)
+          throw "Error while writing to file";
+    if(fwrite (preflopEhsSquaredTable.c_str()
+            , sizeof(char)
+            , preFlopEhsTableSize
+            , out )
+        !=preFlopEhsTableSize)
+          throw "Error while writing to file";
+    if(fwrite (flopEhsTable.c_str()
+            , sizeof(char)
+            , FlopEhsTableSize
+            , out )
+        !=FlopEhsTableSize)
+          throw "Error while writing to file";
+    if(fwrite (flopEhsSquaredTable.c_str()
+            , sizeof(char)
+            , FlopEhsTableSize
+            , out )
+       !=FlopEhsTableSize)
+         throw "Error while writing to file";
+    if(fwrite (turnEhsTable.c_str()
+            , sizeof(char)
+            , turnEhsTableSize
+            , out )
+        !=turnEhsTableSize
+          throw "Error while writing to file";
+    if(fwrite (turnEhsSquaredTable.c_str()
+            , sizeof(char)
+            , turnEhsTableSize
+            , out )
+        !=turnEhsTableSize)
+          throw "Error while writing to file";
+    if(fwrite (riverEhsTable.c_str()
+            , sizeof(char)
+            , riverEhsTableSize
+            , out )
+        !=riverEhsTableSize)
+          throw "Error while writing to file";;
+   if(fwrite (riverEhsSquaredTable.c_str()
+            , sizeof(char)
+            , riverEhsTableSize
+            , out )
+       !=riverEhsTableSize)
+         throw "Error while writing to file";
+    fclose(out);
 }
 
 
 void EhsLutStorage::load(std::shared_ptr<CardsAbstractionProvider>& cardsAbstractionProvider,
                          const std::string& file_path) {
-    //    std::ifstream ifile("output.txt");  //if file existes
-    //    if(!ifile){
-    (new EhsLutBuilder(cardsAbstractionProvider, *this))->buildAll();
-//}
-//    else {
-////        TODO get strings form file
-//        load_from_string(preflopEhsStr,preflop_ehs_table);
-//        load_from_string(preflopEhsSquaredStr,preflop_ehs_squared_table);
-//
-//        load_from_string(flopEhsStr,flop_ehs_table);
-//        load_from_string(flopEhsSquaredStr,flop_ehs_squared_table);
-//
-//        load_from_string(turnEhsStr,turn_ehs_table);
-//        load_from_string(turnEhsSquaredStr,turn_ehs_squared_table);
-//
-//        load_from_string(riverEhsStr,river_ehs_table);
-//        load_from_string(riverEhsSquaredStr,river_ehs_squared_table);
-//    }
+    FILE* in=fopen((const char *) file_path, "r");  //tries to open file
+    if(in==NULL){                   //if file not exists existes
+        throw "File is not existing";
 
+    }
+    else{
+        uint32_t  prefTableSize
+                , flopTableSize
+                , turnTableSize
+                , riverTableSize;
+        std::fread(reinterpret_cast<char*>(&prefTableSize)
+                , sizeof(prefTableSize),1,in);
+
+        std::fread(reinterpret_cast<char*>(&flopTableSize)
+                , sizeof(flopTableSize),1,in);
+
+        std::fread(reinterpret_cast<char*>(&turnTableSize)
+                , sizeof(turnTableSize),1,in);
+
+        std::fread(reinterpret_cast<char*>(&riverTableSize)
+                , sizeof(riverTableSize),1,in);
+        char buffer0[prefTableSize]
+           , buffer1[prefTableSize]
+           , buffer2[flopTableSize]
+           , buffer3[flopTableSize]
+           , buffer4[turnTableSize]
+           , buffer5[turnTableSize]
+           , buffer6[riverTableSize]
+           , buffer7[riverTableSize];
+
+
+        std::fread(buffer0, sizeof(char)
+                ,prefTableSize,in);
+        std::fread(buffer1, sizeof(char)
+                ,prefTableSize,in);
+
+        std::fread(buffer2, sizeof(char)
+                ,flopTableSize,in);
+        std::fread(buffer3, sizeof(char)
+                ,flopTableSize,in);
+
+        std::fread(buffer4, sizeof(char)
+                ,turnTableSize,in);
+        std::fread(buffer5, sizeof(char)
+                ,turnTableSize,in);
+        std::fread(buffer6, sizeof(char)
+                ,riverTableSize,in);
+        std::fread(buffer7, sizeof(char)
+                ,riverTableSize,in);
+        std::string preflopEhsStr       (buffer0)
+                  , preflopEhsSquaredStr(buffer1)
+                  , flopEhsStr          (buffer2)
+                  , flopEhsSquaredStr   (buffer3)
+                  , turnEhsStr          (buffer4)
+                  , turnEhsSquaredStr   (buffer5)
+                  , riverEhsStr         (buffer6)
+                  , riverEhsSquaredStr  (buffer7);
+
+        load_from_string(preflopEhsStr,preflop_ehs_table);
+        load_from_string(preflopEhsSquaredStr,preflop_ehs_squared_table);
+
+        load_from_string(flopEhsStr,flop_ehs_table);
+        load_from_string(flopEhsSquaredStr,flop_ehs_squared_table);
+
+        load_from_string(turnEhsStr,turn_ehs_table);
+        load_from_string(turnEhsSquaredStr,turn_ehs_squared_table);
+
+        load_from_string(riverEhsStr,river_ehs_table);
+        load_from_string(riverEhsSquaredStr,river_ehs_squared_table);
+    }
+    fclose(in);
 }
 
 void EhsLutStorage::save_to_string(std::unordered_map<uint32_t ,double >& table,
@@ -73,13 +197,13 @@ void EhsLutStorage::load_from_string(std::string& str,
     uint32_t count =0;
     while(count != size){
 
-       uint32_t isoNum;
-       memcpy(&isoNum,&str[0]+count * step, sizeof(uint32_t));
+        uint32_t isoNum;
+        memcpy(&isoNum,&str[0]+count * step, sizeof(uint32_t));
 
-       double ehsVal;
-       memcpy(&ehsVal,&str[0]+count * step + sizeof(uint32_t), sizeof(double));
+        double ehsVal;
+        memcpy(&ehsVal,&str[0]+count * step + sizeof(uint32_t), sizeof(double));
 
-       table_out.insert({{isoNum,ehsVal}});
-       count++;
+        table_out.insert({{isoNum,ehsVal}});
+        count++;
     }
 }
